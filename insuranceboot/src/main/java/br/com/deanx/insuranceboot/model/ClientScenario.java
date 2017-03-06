622a4e7868d67a039b254e706169fa4d299fd7b1
Alex Costa <alex@deanx.com.br>
2017-03-06 17:27:40 +0100

    creating a clientScenario model

--- insuranceboot/src/main/java/br/com/deanx/insuranceboot/model/ClientScenario.java
+++ insuranceboot/src/main/java/br/com/deanx/insuranceboot/model/ClientScenario.java
@@ -1,8 +1,21 @@
 package br.com.deanx.insuranceboot.model;
 
+import java.math.BigDecimal;
+
 public class ClientScenario {
-	public static void main(String[] args) {
+	private BigDecimal itemValue;
+	private String itemType;
 	
+	public BigDecimal getItemValue() {
+		return itemValue;
 	}
-	
+	public void setItemValue(BigDecimal itemValue) {
+		this.itemValue = itemValue;
+	}
+	public String getItemType() {
+		return itemType;
+	}
+	public void setItemType(String itemType) {
+		this.itemType = itemType;
+	}	
 }
