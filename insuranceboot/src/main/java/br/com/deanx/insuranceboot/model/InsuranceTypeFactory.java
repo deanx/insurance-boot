2e62ed2a327a0e9a6fadfd0fb3adce5be88f8ec7
Alex Costa <alex@deanx.com.br>
2017-03-06 21:43:43 +0100

    making factories throw exceptions

--- insuranceboot/src/main/java/br/com/deanx/insuranceboot/model/InsuranceTypeFactory.java
+++ insuranceboot/src/main/java/br/com/deanx/insuranceboot/model/InsuranceTypeFactory.java
@@ -2,21 +2,22 @@ package br.com.deanx.insuranceboot.model;
 
 public class InsuranceTypeFactory {
 	public InsuranceType getInsuranceType(String productType) {
-		if(productType.equals(InsuranceType.BICICLE)) {
+		if(productType.equals(InsuranceType.BICICLE.toString())) {
 			return InsuranceType.BICICLE;
 		}
 		
-		if(productType.equals(InsuranceType.JEWELRY)) {
+		if(productType.equals(InsuranceType.JEWELRY.toString())) {
 			return InsuranceType.JEWELRY;
 		}
 		
-		if(productType.equals(InsuranceType.ELECTRONICS)) {
+		if(productType.equals(InsuranceType.ELECTRONICS.toString())) {
 			return InsuranceType.ELECTRONICS;
 		}
 		
-		if(productType.equals(InsuranceType.SPORTS_EQUIPMENT)) {
+		if(productType.equals(InsuranceType.SPORTS_EQUIPMENT.toString())) {
 			return InsuranceType.SPORTS_EQUIPMENT;
 		}
-		return null;
+		
+		throw new IllegalArgumentException();
 	}
 }
--- insuranceboot/src/main/java/br/com/deanx/insuranceboot/service/InsuranceServiceFactory.java
+++ insuranceboot/src/main/java/br/com/deanx/insuranceboot/service/InsuranceServiceFactory.java
@@ -1,24 +1,29 @@
 package br.com.deanx.insuranceboot.service;
 
+import br.com.deanx.insuranceboot.model.ClientScenario;
 import br.com.deanx.insuranceboot.model.InsuranceType;
 
 public class InsuranceServiceFactory {
-	public InsuranceService getInsuranceService(String productType) {
-		productType = productType.toUpperCase();
+	public InsuranceService getInsuranceService(ClientScenario clientScenario) {
+		if(null == clientScenario || null == clientScenario.getItemType()) {
+			throw new IllegalArgumentException();
+		}
 		
-		if (productType.equals(InsuranceType.BICICLE.toString())) {
+		String itemType = clientScenario.getItemType().toUpperCase();
+		
+		if (itemType.equals(InsuranceType.BICICLE.toString())) {
 			return new BikeInsuranceService();
 		}
 
-		if (productType.equals(InsuranceType.JEWELRY.toString())) {
+		if (itemType.equals(InsuranceType.JEWELRY.toString())) {
 			return new JewelryInsuranceService();
 		}
 
-		if (productType.equals(InsuranceType.ELECTRONICS.toString())) {
+		if (itemType.equals(InsuranceType.ELECTRONICS.toString())) {
 			return new ElectronicsInsuranceService();
 		}
 
-		if (productType.equals(InsuranceType.SPORTS_EQUIPMENT.toString())) {
+		if (itemType.equals(InsuranceType.SPORTS_EQUIPMENT.toString())) {
 			return new SportsEquipmentInsuranceService();
 		}
 
